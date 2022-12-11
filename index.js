a529bec19575a89ebe60d8944ebc6b436c76d709
Tanmay Sarkar <sarkartanmay393@gmail.com>
2022-12-11 21:47:55 +0530

    Close to final 🌟

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let input_el = document.getElementById("input-el");
+let input_el = document.getElementById("input_el");
 let ul_el = document.getElementById("ul_el");
 
 let saveInputButton = document.getElementById("save-input-btn");
@@ -10,13 +10,29 @@ let inputsList = [];
 
 
 saveInputButton.addEventListener("click", saveInput);
-saveLinkButton.addEventListener("click", () => { });
+saveLinkButton.addEventListener("click", saveLink);
 clearAllButton.addEventListener("click", clearAll);
+input_el.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        saveInput();
+    }
+});
 
 // To save written text input in list.
 function saveInput() {
-    inputsList.push(input_el.innerText);
+    if (input_el.value == "") {
+        return
+    }
+    inputsList.push(input_el.value);
+    input_el.value = "";
+
     updateListInputs();
+}
+
+// To save link directly from current tab.
+function saveLink() {
+
 }
 
 
