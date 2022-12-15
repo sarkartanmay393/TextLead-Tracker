6299d8a8035a726f5c7893506ecbec97c2bd8098
Tanmay Sarkar <sarkartanmay393@gmail.com>
2022-12-15 23:15:13 +0530

    Starting off new feature implementation!

--- index.js
+++ index.js
@@ -17,57 +17,22 @@ if (inputsListJSON) {
     updateListInputs();
 }
 
-// To update list inputs on screen.
-function updateListInputs() {
-    let mobile = false;
-    let listItem = "";
-    for (let i = 0; i < inputsList.length; i++) {
-        if (inputsList[i] != "") {
-            if (window.outerWidth < 450) {
-                mobile = true;
-                listItem += `
-                <li id="list-item-${i}">
-                    <div class="list-item-el">
-                        <p id="text-to-copy-${i}"> <a href="javascript:copiedText(${i});">${inputsList[i]}</a></p>
-                        <button id="delete-text-btn-mobile">
-                            <a href="javascript:deleteText(${i}, ${mobile});"><i class="fa fa-close"></i></a>
-                        </button>
-                     </div>
-                </li>`
-                    ;
-            } else {
-                listItem += `
-                <li id="list-item-${i}">
-                    <div class="list-item-el">
-                        <p id="text-to-copy-${i}"> <a href="javascript:copiedText(${i});">${inputsList[i]}</a></p>
-                        <div class="buttons-div list-item-btn-div">
-                            <div class="tooltip">
-                                <button id="copy-text-btn"  onmouseout="outFuncC()" onclick="copiedText(${i})">
-                                    <span class="tooltiptext copyTooltip" id="myTooltip">
-                                        Copy to clipboard
-                                    </span>
-                                    Copy
-                                </button>
-                            </div>
-                            <div class="tooltip">
-                                <button id="delete-text-btn" onmouseout="outFuncD()" onclick="deleteText(${i}, ${mobile})" >
-                                    <span class="tooltiptext deleteTooltip" id="myTooltip">
-                                        Delete the lead
-                                    </span>
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </li>`
-                    ;
-            }
-        }
-    }
-    ul_el.innerHTML = listItem;
+let copyBtn = document.getElementById("copy-text-btn");
+let deleteBtn = document.getElementById("delete-text-btn");
+let isMobile = false;
 
-    // putting all text leads in local storage.
-    localStorage.setItem("inputsList", JSON.stringify(inputsList));
+if (window.outerWidth <= 400) {
+    isMobile = true;
+}
+
+if (inputsList.length > 0) {
+    if (window.outerWidth > 400) {
+        copyBtn.addEventListener("mouseout", outFuncC);
+        copyBtn.addEventListener("click", copiedText(copyBtn.value));
+
+        deleteBtn.addEventListener("mouseout", outFuncD);
+        deleteBtn.addEventListener("click", deleteText(deleteBtn.value)); s
+    }
 }
 
 saveInputButton.addEventListener("click", saveInput);
@@ -87,17 +52,78 @@ function saveInput() {
     }
     inputsList.push(input_el.value);
     input_el.value = "";
+
     updateListInputs();
 }
 
 // To save link directly from current tab.
 function saveLink() {
+    let tab = chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    console.log(tab);
+}
+
+let list_el = document.getElementById("list-item");
+if (window.outerWidth <= 400) {
+    document.getElementById("text-lead-el").addEventListener("click", copiedText(list_el.value));
+}
+document.getElementById("delete-lead-el").addEventListener("click", deleteText(list_el.value));
+
+// To update list inputs on screen.
+function updateListInputs() {
+    let listItem = "";
+    for (let i = 0; i < inputsList.length; i++) {
+        if (inputsList[i] != "") {
+            if (window.outerWidth <= 400) {
+                listItem += `
+                <li id="list-item" value="${i}">
+                    <div class="list-item-el">
+                        <p id="text-to-copy-${i}"> <a id="text-lead-el">${inputsList[i]}</a></p>
+                        <button id="delete-text-btn-mobile">
+                            <a id="delete-lead-el"><i class="fa fa-close"></i></a>
+                        </button>
+                     </div>
+                </li>`
+                    ;
+            } else {
+                listItem += `
+                <li id="list-item" value="${i}">
+                    <div class="list-item-el">
+                        <p id="text-to-copy-${i}"> <a id="text-lead-el">${inputsList[i]}</a></p>
+                        <div class="buttons-div list-item-btn-div">
+                            <div class="tooltip">
+                                <button id="copy-text-btn" value="${i}">
+                                    <span class="tooltiptext copyTooltip" id="myTooltip">
+                                        Copy to clipboard
+                                    </span>
+                                    Copy
+                                </button>
+                            </div>
+                            <div class="tooltip">
+                                <button id="delete-text-btn" value="${i}">
+                                    <span class="tooltiptext deleteTooltip" id="myTooltip">
+                                        Delete the lead
+                                    </span>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </li>`
+                    ;
+            }
+        }
+    }
+    ul_el.innerHTML = listItem;
+
+    // putting all text leads in local storage.
+    localStorage.setItem("inputsList", JSON.stringify(inputsList));
 }
 
 // To clear all inputs.
 function clearAll() {
     inputsList = [];
     localStorage.clear();
+
     updateListInputs();
 }
 
@@ -114,11 +140,13 @@ function copiedText(id) {
     window.getSelection().removeAllRanges();
 
 
-    let tooltip = document.getElementsByClassName("copyTooltip");
-    tooltip.item(0).textContent = "Copied: " + temp.textContent;
+    if (!isMobile) {
+        let tooltip = document.getElementsByClassName("copyTooltip");
+        tooltip.item(0).textContent = "Copied: " + temp.textContent;
+    }
 }
 
-function deleteText(id, mobile) {
+function deleteText(id) {
     let tempInputsList = [];
     inputsList.forEach(function (value, index) {
         if (!(index == id)) {
@@ -127,7 +155,7 @@ function deleteText(id, mobile) {
     });
     inputsList = tempInputsList;
 
-    if (!mobile) {
+    if (!isMobile) {
         let tooltip = document.getElementsByClassName("deleteTooltip");
         tooltip.item(0).textContent = "Deleted!";
     }
